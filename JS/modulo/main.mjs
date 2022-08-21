5ea86c63469f9c3cb47ebb372a5ae0b9f513c21c
Yuri Xavier Figueiredo <100726309+yurixf@users.noreply.github.com>
2022-08-21 15:02:45 -0300

    exercicios

---
+++ JS/modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Yuri", 31))
+console.log(mostraCidade("Yuri", "São Leopoldo"))
+console.log(mostraHobby("Yuri", "Ler"))
